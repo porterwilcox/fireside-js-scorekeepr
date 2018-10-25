97c057230d74e39586d131e60c814a15d5fc0977
Porter Wilcox <wilcox.porter@gmail.com>
2018-10-25 16:59:21 -0600

    scoring works. needs some more safety checks

--- app.js
+++ app.js
@@ -3,25 +3,54 @@
 //
 let player1Score = document.getElementById("player1")
 let player2Score = document.getElementById("player2")
-let myTimer;
+let player1Available = true;
+let player2Available = true;
+let scoreToWin;
+let myTitle = document.getElementById("my-title")
 
 //
 //FUNCTIONS
 //
-function startTimer(num) {
-    myTimer = setTimeout(playerIncrement, 500, num);
-}
-
 function playerIncrement(num) {
     if (num != 1 && num != 2) { return }
     if (num == 1) {
         player1Score.textContent = (parseFloat(player1Score.textContent) + 1).toString();
+        return;
+    }
+    player2Score.textContent = (parseFloat(player2Score.textContent) + 1).toString();
+    if (player1Score.textContent == scoreToWin || player2Score.textContent == scoreToWin) {
+        if (player1Score.textContent == scoreToWin) {
+            myTitle.textContent = "Player 1 wins!"
+            endGame();
+            return;
+        }
+        myTitle.textContent = "Player 2 wins!"
+        endGame()
     }
 }
-function playerDecrement(num) {
-    clearTimeout(myTimer)
+function playerDecrement(e, num) {
     if (num != 1 && num != 2) { return }
-    if (num == 1) {
+    if (num == 1 && e.deltaY < 0 && player1Available && player1Score.textContent != '0') {
+        player1Available = false
+        let wait = setTimeout(() => player1Available = true, 1000)
         player1Score.textContent = (parseFloat(player1Score.textContent) - 1).toString();
     }
+    if (num == 2 && e.deltaY < 0 && player2Available && player2Score.textContent != '0') {
+        player2Available = false
+        let wait = setTimeout(() => player2Available = true, 1000)
+        player2Score.textContent = (parseFloat(player2Score.textContent) - 1).toString();
+    }
+}
+
+function reset() {
+    player1Score.textContent = "0";
+    player2Score.textContent = "0";
+}
+
+function setScoreToWin(target) {
+    scoreToWin = target;
+}
+
+function endGame() {
+    
 }
